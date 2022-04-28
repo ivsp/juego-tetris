142c4bfe65f56b9776a6e9525e6d70e2375d87e0
Iván Sánchez <ivansperez93@gmail.com>
2022-04-28 13:04:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -608,6 +608,7 @@ function updateTetrisBoard() {
 // VOY A EMPEZAR POR MOVER LA PIEZA HACIA LA DERECHA CUANDO PULSAMOS LA FLECHA HACIA LA DERECHA
 
 document.body.onkeydown = function (event) {
+  event.preventDefault();
   switch (event.keyCode) {
     case 37: // Izquierda
       moveLeft(event);
